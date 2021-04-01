138f770c70439c6887f18051bcb4555508cad2f1
Michelle Rodrigues <mica.msr@gmail.com>
2021-04-01 20:54:22 -0300

    inclusão database e initialize

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Telefonia.Crud.Infra.Database.Context;
 
 namespace poc_telefonia_mica
 {
@@ -14,7 +16,9 @@ namespace poc_telefonia_mica
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateDbIfNotExists(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -23,6 +27,26 @@ namespace poc_telefonia_mica
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<TelefoniaDatabaseContext>();
+                    DbTelefoniaInitializer.StartDataBase(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Ocorreu um erro ao criar o Banco de dados de Telefonia");
+                }
+            }
+        }
     }
+
+
     #pragma warning restore CS1591
 }
--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+using Telefonia.Crud.Infra.Database.Context;
 
 namespace poc_telefonia_mica
 {
@@ -53,7 +49,12 @@ namespace poc_telefonia_mica
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
             });
-            
+
+            services.AddDbContext<TelefoniaDatabaseContext>(
+                option => option.UseSqlServer(Configuration.GetConnectionString("Default")),
+                ServiceLifetime.Scoped
+           );
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
