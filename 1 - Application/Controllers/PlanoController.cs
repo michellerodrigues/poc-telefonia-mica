2127ae6f55098013ff153782d1d60da1e5280e66
Michelle Rodrigues <mica.msr@gmail.com>
2021-04-01 21:59:46 -0300

    inclusão controller plano

---
+++ 1 - Application/Controllers/PlanoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Telefonia.Crud.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlanoController : ControllerBase
+    {
+        private readonly ILogger<PlanoController> _logger;
+
+        public PlanoController(ILogger<PlanoController> logger)
+        {
+            _logger = logger;
+        }          
+    }
+}
