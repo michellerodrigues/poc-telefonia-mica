522ee01ae3e06834d72fab52df1e312fac98a1d3
Michelle Rodrigues <mica.msr@gmail.com>
2021-04-02 17:49:48 -0300

    inclusão de middleware para tratamento de exceção global

--- 1 - Application/Startup.cs
+++ 1 - Application/Startup.cs
@@ -11,6 +11,7 @@ using System.Reflection;
 using Telefonia.Crud.Infra.Database.Context;
 using Telefonia.Crud.Infra.Database.Repository;
 using Telefonia.Crud.Services;
+using Telefonia.Crud.WebApi.Middleware;
 
 namespace poc_telefonia_mica
 {
@@ -68,6 +69,7 @@ namespace poc_telefonia_mica
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware(typeof(ErrorHandlingMiddleware));
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
